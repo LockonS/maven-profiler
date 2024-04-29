7025184dfb97f3e7929e0a35e57ab1dbb1e0e9e9
LockonStratos <lockon.stratos@dreamisty.com>
2024-04-29 03:54:30 +0000

    add mvnp-switch and check config file before switch operation

--- maven-profiler.plugin.zsh
+++ maven-profiler.plugin.zsh
@@ -17,92 +17,110 @@ _mvnp_load_env() {
     export MAVEN_PROFILE=''
   fi
 
+  _mvnp_error() {
+    echo -e "[${BIBlue}${MAVEN_PROFILER_PROMPT_TITLE}${NC}] ${BIWhite}---${NC} ${Red}${1}${NC} ${BIWhite} ---${NC}"
+  }
+
+  _mvnp_prompt() {
+    echo -e "[${BIBlue}${MAVEN_PROFILER_PROMPT_TITLE}${NC}] ${BIWhite}---${NC} ${Green}${1}${NC} ${BIWhite}(${2}) ---${NC}"
+  }
+
+  _mvnp_show_profile() {
+    if [[ -n $MAVEN_PROFILE ]]; then
+      _mvnp_prompt "mvn profile" "${MAVEN_PROFILE}"
+    else
+      _mvnp_prompt "mvn profile" "${MAVEN_PROFILER_DEFAULT_PROFILE}"
+    fi
+  }
+
+  _mvnp_switch_profile() {
+    local TARGET_MAVEN_PROFILE=${1}
+    local TARGET_MAVEN_CONFIG_FILE
+    # validate if maven config file exist
+    TARGET_MAVEN_CONFIG_FILE=$(_mvnp_assemble_config_file_path "$TARGET_MAVEN_PROFILE")
+    if [[ ! -f "$TARGET_MAVEN_CONFIG_FILE" ]]; then
+      _mvnp_error "maven config file [$(basename "$TARGET_MAVEN_CONFIG_FILE")] not found, rollback to previous profile"
+      _mvnp_show_profile
+      return 1
+    fi
+    MAVEN_PROFILE=$TARGET_MAVEN_PROFILE
+    _mvnp_show_profile
+  }
+
+  _mvnp_assemble_config_file_path() {
+    local MAVEN_PROFILE=${1}
+    # use default conf file for profile 'default'
+    if [[ "$MAVEN_PROFILE" == "$MAVEN_PROFILER_DEFAULT_PROFILE" ]]; then
+      MAVEN_CONFIG_FILE=$MAVEN_HOME/conf/settings.xml
+    else
+      MAVEN_CONFIG_FILE=$MAVEN_HOME/conf/settings-$MAVEN_PROFILE.xml
+    fi
+    echo "$MAVEN_CONFIG_FILE"
+  }
+
+  _mvnp_autoconfig() {
+    # locate mvn installed on system if MAVEN_HOME is not set
+    if [[ -n $MAVEN_HOME ]]; then
+      echo -e "[maven-profiler] MAVEN_HOME is not empty, abort duplicated setup action"
+      return 0
+    fi
+    if command -v mvn &>/dev/null; then
+      MAVEN_EXECUTABLE=$(which mvn)
+      export MAVEN_HOME="${MP_MAVEN_EXECUTABLE%/*/*}"
+    else
+      echo -e "[maven-profiler] maven is not found in PATH"
+      return 1
+    fi
+  }
+
+  mvn-profiler() {
+    local JDK_VERSION_OUTPUT MAVEN_EXECUTABLE
+    MAVEN_EXECUTABLE=$MAVEN_HOME/bin/mvn
+    MAVEN_CONFIG_FILE=$MAVEN_HOME/conf/settings-$MAVEN_PROFILE.xml
+
+    # validations
+    if [[ -z $MAVEN_PROFILE ]]; then
+      if [[ -n $MAVEN_PROFILE_DEFAULT_OVERRIDE ]]; then
+        # if default maven profile is overrided, apply $MAVEN_PROFILE_DEFAULT_OVERRIDE automatically
+        export MAVEN_PROFILE=$MAVEN_PROFILE_DEFAULT_OVERRIDE
+      else
+        # apply default maven profile
+        export MAVEN_PROFILE=$MAVEN_PROFILER_DEFAULT_PROFILE
+      fi
+    fi
+
+    MAVEN_CONFIG_FILE=$(_mvnp_assemble_config_file_path "$MAVEN_PROFILE")
+
+    if [[ ! -f $MAVEN_CONFIG_FILE ]]; then
+      _mvnp_error "maven config file [$(basename "$MAVEN_CONFIG_FILE")] not found"
+      return 1
+    fi
+
+    if [[ ! -f $MAVEN_EXECUTABLE ]]; then
+      _mvnp_error "maven executable not found, please check if \$MAVEN_HOME variable is properly configured"
+      return 1
+    fi
+
+    # shellcheck disable=SC2207
+    IFS=$'\n' JDK_VERSION_OUTPUT=($(java --version))
+    _mvnp_prompt "jdk version" "${JDK_VERSION_OUTPUT[1]}"
+    _mvnp_prompt "mvn profile" "${MAVEN_PROFILE}"
+    _mvnp_prompt "mvn home" "${MAVEN_HOME}"
+    _mvnp_prompt "mvn conf" "${MAVEN_CONFIG_FILE}"
+
+    # pause for a short time for human eye to catch up with the prompt message
+    sleep 1
+
+    # execute mvn with designate config file and passed options
+    "$MAVEN_EXECUTABLE" -s "$MAVEN_CONFIG_FILE" "$@"
+  }
+
   # override default mvn command
   alias mvn=mvn-profiler
   alias mvnp=mvn-profiler
-}
-
-_mvnp_error() {
-  echo -e "[${BIBlue}${MAVEN_PROFILER_PROMPT_TITLE}${NC}] ${BIWhite}---${NC} ${Red}${1}${NC} ${BIWhite} ---${NC}"
-}
-
-_mvnp_prompt() {
-  echo -e "[${BIBlue}${MAVEN_PROFILER_PROMPT_TITLE}${NC}] ${BIWhite}---${NC} ${Green}${1}${NC} ${BIWhite}(${2}) ---${NC}"
-}
-
-mvn-profiler() {
-  local JDK_VERSION_OUTPUT MAVEN_EXECUTABLE MAVEN_CONFIG_FILE
-  MAVEN_EXECUTABLE=$MAVEN_HOME/bin/mvn
-  MAVEN_CONFIG_FILE=$MAVEN_HOME/conf/settings-$MAVEN_PROFILE.xml
-
-  # validations
-  if [[ -z $MAVEN_PROFILE ]]; then
-    if [[ -n $MAVEN_PROFILE_DEFAULT_OVERRIDE ]]; then
-      # if default maven profile is overrided, apply $MAVEN_PROFILE_DEFAULT_OVERRIDE automatically
-      export MAVEN_PROFILE=$MAVEN_PROFILE_DEFAULT_OVERRIDE
-    else
-      # apply default maven profile
-      export MAVEN_PROFILE=$MAVEN_PROFILER_DEFAULT_PROFILE
-    fi
-  fi
-
-  # use default conf file for profile 'default'
-  if [[ "$MAVEN_PROFILE" == "$MAVEN_PROFILER_DEFAULT_PROFILE" ]]; then
-    MAVEN_CONFIG_FILE=$MAVEN_HOME/conf/settings.xml
-  else
-    MAVEN_CONFIG_FILE=$MAVEN_HOME/conf/settings-$MAVEN_PROFILE.xml
-  fi
-
-  if [[ ! -f $MAVEN_EXECUTABLE ]]; then
-    _mvnp_error "maven executable not found, please check if \$MAVEN_HOME variable is properly configured"
-    return 1
-  fi
-
-  if [[ ! -f $MAVEN_CONFIG_FILE ]]; then
-    _mvnp_error "maven configuration file [$(basename "$MAVEN_CONFIG_FILE")] not found"
-    return 1
-  fi
-
-  # shellcheck disable=SC2207
-  IFS=$'\n' JDK_VERSION_OUTPUT=($(java --version))
-  _mvnp_prompt "jdk version" "${JDK_VERSION_OUTPUT[1]}"
-  _mvnp_prompt "mvn profile" "${MAVEN_PROFILE}"
-  _mvnp_prompt "mvn home" "${MAVEN_HOME}"
-
-  # pause for a short time for human eye to catch up with the prompt message
-  sleep 1
-
-  # execute mvn with designate configuration file and passed options
-  "$MAVEN_EXECUTABLE" -s "$MAVEN_CONFIG_FILE" "$@"
-}
-
-mvnp-show-profile() {
-  if [[ -n $MAVEN_PROFILE ]]; then
-    _mvnp_prompt "mvn profile" "${MAVEN_PROFILE}"
-  else
-    _mvnp_prompt "mvn profile" "${MAVEN_PROFILER_DEFAULT_PROFILE}"
-  fi
-}
-
-mvnp-set-profile() {
-  export MAVEN_PROFILE=${1}
-  mvnp-show-profile
-}
-
-mvnp-autoconfigure() {
-  # locate mvn installed on system if MAVEN_HOME is not set
-  if [[ -n $MAVEN_HOME ]]; then
-    echo -e "[maven-profiler] MAVEN_HOME is not empty, abort duplicated setup action"
-    return 0
-  fi
-  if command -v mvn &>/dev/null; then
-    MAVEN_EXECUTABLE=$(which mvn)
-    export MAVEN_HOME="${MP_MAVEN_EXECUTABLE%/*/*}"
-  else
-    echo -e "[maven-profiler] maven is not found in PATH"
-    return 1
-  fi
+  alias mvnp-switch=_mvnp_switch_profile
 }
 
 _mvnp_load_env
 unfunction _mvnp_load_env
+
